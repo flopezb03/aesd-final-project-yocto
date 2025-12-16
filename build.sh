65fc09551fbcd96343f6ef8977909250df4050f0
flopezb03 <f.lopezberrocal@gmail.com>
2025-12-16 16:57:10 +0100

    Added meta-image layer and build.sh

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+git submodule init
+git submodule sync
+git submodule update
+
+source poky/oe-init-build-env
+
+
+add_layer() {
+    layer=$1
+    bitbake-layers show-layers | grep "$layer" > /dev/null
+    layer_info=$?
+
+    if [ $layer_info -ne 0 ];then
+        echo "Adding $layer layer"
+        bitbake-layers add-layer ../$layer
+    else
+        echo "$layer layer already exists"
+    fi
+}
+
+
+
+CONFLINE="MACHINE = \"raspberrypi3-64\""
+
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+local_conf_info=$?
+
+if [ $local_conf_info -ne 0 ];then
+	echo "Append ${CONFLINE} in the local.conf file"
+	echo ${CONFLINE} >> conf/local.conf
+
+else
+	echo "${CONFLINE} already exists in the local.conf file"
+fi
+
+add_layer "meta-openembedded/meta-oe"
+add_layer "meta-openembedded/meta-python"
+add_layer "meta-openembedded/meta-networking"
+add_layer "meta-raspberry"
+
+add_layer "meta-image"
+
+set -e
+bitbake aesd-rpi-image
+
---
+++ meta-image/recipes-core/images/aesd-rpi-image.bb
@@ -0,0 +1,7 @@
+inherit core-image
+EXTRA_IMAGE_FEATURES += "debug-tweaks ssh-server-openssh"
+IMAGE_INSTALL += " \
+    wireless-regdb-static \
+    connman \
+    connman-client \
+    "
